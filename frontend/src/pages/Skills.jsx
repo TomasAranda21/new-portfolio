6353a03c1e64f790e4c430ea8951e16a91a86f36
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-04 19:27:40 -0300

    add section skills and about

---
+++ frontend/src/pages/Skills.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+
+    <section>
+
+        <div>
+            <h2>Skills & Experiencia</h2>
+
+        </div>
+
+
+    </section>
+  )
+}
+
+export default Skills
