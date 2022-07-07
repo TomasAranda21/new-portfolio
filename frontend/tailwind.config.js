46f4ec64dbf6fe1b6164c3ffe005db3c3417d3a7
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-07 19:39:35 -0300

    add dependencie free icon, and add tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,13 +11,26 @@ module.exports = {
 
       colors: {
         amarillo: '#FFEFAE',
+        naraja: "#EAA664",
         rosaClaro: '#FFC4A9',
         rosaOscuro: '#FA8D74',
         rojo: "#BE5944",
         rojoOscuro: "#670000",
         verdeoOscuro: "#62C8B7",
         verdeClaro: "#7AE4D9",
-        grisAzulado: "#96B1AD",
+        
+        grisAzulado: "96B1AD",
+        
+        
+        
+        verdeClaro1: "#7EECCB",
+        verdeClaro2: "#91F3B7",
+        verdeClaro3: "#AEF89F",
+        verdeClaro4: "#D1F986",
+        azul1: "#36ADE4",
+        azul2: "#628AD4",
+        azul3: "#8864AF",
+        azul4: "#993B79",
       
       },
 
