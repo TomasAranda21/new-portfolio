afc33181579f96c20df350ca31ea9fb25ea66b9b
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-07 19:46:15 -0300

    change bg color and add separator

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,40 +4,45 @@ import ButtonAbout from '../components/buttons/ButtonAbout'
 const About = () => {
   return (
 
-    <section className="relative py-40">
+    <section className="">
 
-      <div className="flex justify-center gap-20 relative z-10 items-center">
+      <div className="relative pt-40 w-5/6 mx-auto">
+        <div className="flex justify-center gap-20 relative z-10 items-center">
 
 
-        <div className="w-1/3 flex flex-col gap-5 ">
+          <div className="w-1/3 flex flex-col gap-5 font-medium ">
 
-          <h1 className="text-3xl font-fontTitles uppercase font-bold">Sobre mi</h1>
-          <p>A mi corta edad realicé un negocio online que me permitió comprar mi primera computadora y pude encontrar este maravilloso mundo de la programación. Lo que al principio solo fue un pasa tiempo, con estudio y dedicación se convirtió en una de las cosas que más amo hacer en mi día a día. Por lo que siempre trabajo para lograr el mejor resultado en cada proyecto que tengo en mis manos.</p>
+            <h1 className="text-3xl font-fontTitles uppercase font-bold">Sobre mi</h1>
+            <p>A mi corta edad realicé un negocio online que me permitió comprar mi primera computadora y pude encontrar este maravilloso mundo de la programación. Lo que al principio solo fue un pasa tiempo, con estudio y dedicación se convirtió en una de las cosas que más amo hacer en mi día a día. Por lo que siempre trabajo para lograr el mejor resultado en cada proyecto que tengo en mis manos.</p>
+            
+            <p>Lo próximo que quiero en mi vida es tener una oportunidad laboral en el que pueda colaborar con mis conocimientos, además me gustaría mucho poder compartir con otras personas y aprender de ellas para poder seguir creciendo como persona y como profesional.</p>
           
-          
-          <p>Lo próximo que quiero en mi vida es tener una oportunidad laboral en el que pueda colaborar con mis conocimientos, además me gustaría mucho poder compartir con otras personas y aprender de ellas para poder seguir creciendo como persona y como profesional.</p>
-        
             <ButtonAbout text="Descargar CV"/>
-
-        </div>
-
-        <div className=' overflow-hidden shadow-xl rounded-full relative bg-rosaClaro'>
-          <img className=" object-cover" src="https://tomasaranda.com/assets/fotoPerfil.b9fa0696.webp" alt="" width="340" />
-
-        </div>
-
-
-
-      </div>
-
-          <div className="bg_img border-2">
 
           </div>
 
-      <div className='absolute z-0 text-center text-gray-100 text-10xl font-bold top-0 left-1/3'>
-        <h2>About</h2>
+          <div className='overflow-hidden  rounded-full relative bg-gray-50 '>
+            <img className="object-cover" src="https://tomasaranda.com/assets/fotoPerfil.b9fa0696.webp" alt="" width="340" />
+
+          </div>
+
+
+        </div>
+
+        <div className='absolute z-0 text-center text-gray-100 text-10xl font-bold top-0 left-1/3'>
+          <h2>About</h2>
+
+        </div>
 
       </div>
+
+    <div className="block">
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 280">
+        <path fill="#f1f5f9" fillOpacity="1" d="M0,224L48,229.3C96,235,192,245,288,213.3C384,181,480,107,576,112C672,117,768,203,864,229.3C960,256,1056,224,1152,186.7C1248,149,1344,107,1392,85.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+      </svg>
+
+    </div>
 
     </section>
 
