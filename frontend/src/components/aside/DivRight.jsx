a62a1fdbe0d50235a201b69a1e5cf1a6f20de794
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-07 19:43:48 -0300

    add socials medias and email

---
+++ frontend/src/components/aside/DivRight.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const DivRight = () => {
+
+  return (
+    <div  className="w-full m-0 p-0 text-2xl  hidden 2xl:block">
+
+    <div className="fixed w-1/6 right-0 bottom-0">
+        
+        <div 
+            className="text-lg hidden md:flex md:flex-col md:items-center text-gray-900 ml-20 mb-10">
+                <div className='font_mail rotate-90 block hover:text-cyan-500'>
+                    <a href="mailto:tomasaranda2018@gmail.com" className=""><h2>tomasaranda2018@gmail.com</h2></a>
+                </div>
+            <div className='border border-l-3 rotate-90 w-24 mt-44 mb-6 border-gray-400'></div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default DivRight
