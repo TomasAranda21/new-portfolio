1e8fa3de042cdb87b775d10ed8a4d48493a054ce
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-06 22:48:56 -0300

    change name and add buttons in css

---
+++ frontend/src/components/buttons/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './button.css'
+
+const Button = ({text, type}) => {
+  return (
+
+    <button className="button_principal 
+    border uppercase rounded-sm border-l-4 p-3 border-b-4 border-rosaOscuro hover:border-slate-200  
+    text-rojoOscuro font-bold
+    hover:text-gray-900" 
+    type={type}
+    >{text}</button>
+
+  )
+}
+
+export default Button
