5a7ebec1e58def0e931da53909c05cc5ff175404
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-07 19:46:49 -0300

    add buttons experiencie and education

---
+++ frontend/src/components/buttons/ButtonSkills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ButtonSkills = ({text}) => {
+
+  return (
+
+    <button className='px-10 p-3 border-2 rounded-lg  text-white font-bold focus:bg-naraja'>{text}</button>
+
+  )
+
+}
+
+export default ButtonSkills
