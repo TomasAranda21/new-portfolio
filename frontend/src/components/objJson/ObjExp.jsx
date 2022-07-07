c11f662a7fd8f6d9b8de5f6e9c6aea33da35d5eb
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-07 19:43:06 -0300

    add 'json' to test to styles

---
+++ frontend/src/components/objJson/ObjExp.jsx
@@ -0,0 +1,48 @@
+
+const ObjExp = [
+
+    {   
+        skills: [
+            "React", "Node JS", "Express", "React Router Dom", "Heroku", "Tailwind"
+        ],
+        
+        title: "Administrador de Turnos",
+
+        description: `El cliente necesitaba de un administrador de turnos para poder agendar a sus clientes y poder editarlos o 
+        borrarlos cuando él lo precise.`,
+
+
+        link: "http://admin-turnos-peluqueria.mi-site.online",
+
+        client : "Peluquería Marga",
+        date: 'De Febrero 2022 a Marzo 2022',
+
+        id: 1
+
+    },
+
+    {   
+
+        skills: [
+            "React", "Node JS", "Express", "React Router Dom", "Heroku", "Tailwind"
+        ],
+
+        title: "E-commerce y Admin para Verduleria",
+
+        description: `El cliente requería de una tienda para su verdulería. Adicionalmente, necesitaba una web para poder editar, crear y eliminar sus productos.`,
+        
+        link: "http://tienda-verduleria-luz.mi-site.online",
+        
+        client : "Verdulería Luz",
+        date: 'De Marzo 2022 a Abril 2022',
+        
+        id: 2
+
+
+    },
+
+]
+
+
+export default ObjExp
+
---
+++ frontend/src/components/objJson/skills.jsx
@@ -0,0 +1,37 @@
+const ObjSkill = [
+
+    {   
+        type: "Frontend",
+        skills: [
+            "Html",
+            "CSS",
+            "JavaScript",
+            "React",
+            "NextJs",
+            "Bootstrap",
+            "Tailwind"
+        ],
+        id: 1,
+        
+    },
+
+    {   
+        type: "Backend",
+        skills: [
+            "NodeJS",
+            "MongoDB",
+            "Firebase",
+            "Git"
+        ],
+        
+        id: 2
+        
+    },
+
+]
+
+
+
+
+
+export default ObjSkill
