5c1e952d8aeac3f45dfc4f0afb117362d322eac9
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-10 00:04:41 -0300

    change bg color

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -8,17 +8,14 @@ const Footer = () => {
   return (
 
 
-        <footer  className="bg-verdeClaro1"
-        >
-        
-
+        <footer  className="bg-verdeClaro1">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
           <path fill="#f9fafb" fillOpacity="1" d="M0,96L60,106.7C120,117,240,139,360,144C480,149,600,139,720,122.7C840,107,960,85,1080,85.3C1200,85,1320,107,1380,117.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
         </svg>
 
             <div className="pb-5"
             >
-                <div className="flex justify-center 2xl:hidden">
+                <div className="flex justify-center 2xl:hidden pb-3">
                     <ul className="flex text-3xl text-gray-800 gap-10 items-center">
                         <a target="_blank" href="https://www.linkedin.com/in/tomas--aranda"><li className="hover:text-cyan-500"><FontAwesomeIcon icon={faLinkedinIn}/></li></a>
                         <a target="_blank" href="https://github.com/TomasAranda21"><li className="hover:text-cyan-500"><FontAwesomeIcon icon={faGithub} className=""/></li></a>
