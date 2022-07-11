54ccaae45d71aa1bf2268ae80bf572572f5020a0
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-11 20:46:27 -0300

    change img card and change z-index

--- frontend/src/components/portfolio/Cards.jsx
+++ frontend/src/components/portfolio/Cards.jsx
@@ -8,8 +8,8 @@ const CardsPortfolio = () => {
 
     <div className="cards">
         <img className=""
-        // src="https://res.cloudinary.com/dkxm9njd6/image/upload/v1654281584/zuser_eygodc.png" alt="" width={400}/>
-        src="https://i.pinimg.com/564x/a7/46/f7/a746f74e09c6684cf3542650e5bb8c4d.jpg" alt="" width={300}/>
+        
+        src="https://res.cloudinary.com/dj1pp4ivb/image/upload/v1657248126/prueba3_qpsdck.png" alt="" width={300}/>
 
         <div className="info_container">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
