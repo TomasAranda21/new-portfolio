52e5958c0edb2e699d92da5145b826fe45b7b547
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-11 20:52:26 -0300

    add link react scroll

--- frontend/src/components/header/Links.jsx
+++ frontend/src/components/header/Links.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-scroll'
 
-const Links = ({text}) => {
+const Links = ({nav, text, texto_links, onClick }) => {
+
   return (
+    <>
+    {/* <div className="flex flex-col items-center font_nav "> */}
+      <Link to={nav} activeClass="active" spy={true} smooth={true} offset={-80} duration={1300} isDynamic={true} delay={400} 
+      onClick={onClick}
+      className="nav_link"> 
+          {text}
+      </Link>
 
-    <a href="#" className="nav_link">{text}</a>
-
-  )
+      <h3 className="block md:hidden capitalize">{texto_links}</h3>
+    {/* </div> */}
+    </>
+    )
 }
 
 export default Links
