66e3dcdc0066ec667112128bb3ec1a50ec9b1c5b
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-14 00:48:46 -0300

    add new styles to cards

--- frontend/src/components/portfolio/Cards.jsx
+++ frontend/src/components/portfolio/Cards.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import './cards.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowUpRightFromSquare} from '@fortawesome/free-solid-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+
 
 const CardsPortfolio = ({img, name, description, linkWeb, linkGit}) => {
   return (
 
-    <div className="relative overflow-hidden rounded-md shadow-xl ">
+    <div className="relative overflow-hidden rounded-md shadow-xl border-2 border-sky-500 duration-300 hover:shadow-sky-200">
 
-    <div className="cards">
+    <div className="cards ">
   
-        <img className=""
-        
-        src={img} alt="" width={300}/>
+        <img className="" src={img} alt="" width={300}/>
 
         <div className="info_container">
-            <h1>{name}</h1>
-            <p>{description}</p>
-            <p>Link</p>
-            <a href={linkWeb} target="_blank" > Link web</a>
-            <a href={linkGit} target="_blank" > Codigo </a>
+            <h1 className="uppercase underline underline-offset-2">{name}</h1>
+            <p className="text-gray-200 font-normal">{description}</p>
+
+            <div className="flex gap-4 items-center justify-center text-gray-300">
+              <a className="hover:text-red-300" href={linkGit} target="_blank" > <FontAwesomeIcon icon={faGithub} className=""/> </a>
+              <a className="hover:text-naraja duration-300" href={linkWeb} target="_blank" > <FontAwesomeIcon icon={faArrowUpRightFromSquare}/> Link web</a>
+            </div>
         </div>
 
     </div>
