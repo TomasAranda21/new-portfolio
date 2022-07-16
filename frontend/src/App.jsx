c0ff7a3b25ab31cb44a799dabf13dfe44a3e6022
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-16 18:09:51 -0300

    add loading page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,78 +11,94 @@ import DivRight from './components/aside/DivRight'
 
 import { collection, addDoc, getDocs, doc,onSnapshot,  } from "firebase/firestore";
 import {db} from './Firebase'
+import LoadingPage from './components/loading/LoadingPage'
 
 
 function App() {
 
   const [dark, isDark] = useState(false)
 
+
   const [experiencia, setExperiencia] = useState([])
+
   
   const [proyects, setProyects] = useState([])
   
   const [skills, setSkills] = useState([])
 
-  useEffect(() => {
+  const [loading, setLoading] = useState(true)
 
+
+  
+  useEffect(() => {
+    
+    setTimeout(() => {
+      
+      setLoading(false)
+      
+    }, 1500)
+    
+    
     const getUser = onSnapshot(
-        collection(db, "user"),
-        (snapShot) => {
-          let list = [];
-          snapShot.docs.forEach((doc) => {
-            list.push({ id: doc.id, ...doc.data() });
-          });
-            
-          setExperiencia(list[0].experiencia)
-          setProyects(list[0].proyects)
-          setSkills(list[0].skills)
+      collection(db, "user"),
+      (snapShot) => {
+        let list = [];
+        snapShot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        
+        setExperiencia(list[0].experiencia)
+        setProyects(list[0].proyects)
+        setSkills(list[0].skills)
         },
 
         (error) => {
           console.log(error);
         }
-      );
-  
-      return () => {
+        );
+        
+        return () => {
+          
+          getUser();
+          
+        };
+      }, [])
+      
+      
+      useEffect(() => {
+        
+        if (dark === true) {
+          
+          document.documentElement.classList.add('dark')
+          
+          console.log("Modo dark")
+        } else {
+          
+          document.documentElement.classList.remove('dark')
+          console.log("Modo light")
+        }
 
-        getUser();
+      }, [dark])
+      
+      // if(loading) return <LoadingPage/>
 
-      };
-}, [])
 
 
-  useEffect(() => {
-
-    if (dark === true) {
-
-      document.documentElement.classList.add('dark')
-
-      console.log("Modo dark")
-    } else {
-
-      document.documentElement.classList.remove('dark')
-      console.log("Modo light")
-    }
-
-  }, [dark])
-
-  return (
-
-    <>
+      
+      return (
+        
+        <>
     
       <Header dark={dark} isDark={isDark}/>
+          <DivLeft/>
+          <DivRight/>
           <About dark={dark}/>
           <Portfolio proyects={proyects} dark={dark}/>
           <Skills experiencia={experiencia} skills={skills} dark={dark}/>
           <Contact dark={dark}/>
 
-          <DivLeft/>
-          <DivRight/>
 
         <Footer dark={dark}/>
-    
-    
-    
     
     </>
     
