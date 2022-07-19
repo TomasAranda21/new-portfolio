efcfcb8affe2ff85a99f96ea2958b70561e4e87b
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-18 21:45:54 -0300

    change card style

--- frontend/src/components/portfolio/Cards.jsx
+++ frontend/src/components/portfolio/Cards.jsx
@@ -8,19 +8,19 @@ import {faGithub} from '@fortawesome/free-brands-svg-icons'
 const CardsPortfolio = ({img, name, description, linkWeb, linkGit}) => {
   return (
 
-    <div className="relative overflow-hidden rounded-md shadow-xl border-2 border-sky-500 duration-300 hover:shadow-sky-200">
+    <div className="relative w-full object-cover overflow-hidden rounded-md shadow-xl border-2 border-sky-500 duration-300 hover:shadow-sky-200">
 
     <div className="cards ">
   
         <img className="" src={img} alt="" width={300}/>
 
         <div className="info_container">
-            <h1 className="uppercase underline underline-offset-2">{name}</h1>
+            <h1 className="text-stone-200 font-bold uppercase underline underline-offset-2">{name}</h1>
             <p className="text-gray-200 font-normal">{description}</p>
 
             <div className="flex gap-4 items-center justify-center text-gray-300">
-              <a className="hover:text-red-300" href={linkGit} target="_blank" > <FontAwesomeIcon icon={faGithub} className=""/> </a>
-              <a className="hover:text-naraja duration-300" href={linkWeb} target="_blank" > <FontAwesomeIcon icon={faArrowUpRightFromSquare}/> Link web</a>
+              <a className="text-gray-200 duration-300 hover:text-gray-400" href={linkGit} target="_blank" > <FontAwesomeIcon icon={faGithub} className=""/> </a>
+              <a className="text-naraja hover:text-amber-600 duration-300" href={linkWeb} target="_blank" > <FontAwesomeIcon icon={faArrowUpRightFromSquare}/> Link web</a>
             </div>
         </div>
 
