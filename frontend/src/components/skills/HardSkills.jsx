deef2079c0fa01ce7d98c7225a25ef6ad93642a1
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-19 22:25:11 -0300

    fix flex-wrap error

--- frontend/src/components/skills/HardSkills.jsx
+++ frontend/src/components/skills/HardSkills.jsx
@@ -6,8 +6,8 @@ import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 const HardSkills = ({text}) => {
   return (
     <div 
-    className="flex gap-2 md:text-base hover:text-azul4 duration-300 text-sm
-    hover:cursor-default cursor-default ease-in-out items-center mx-auto w-1/2">
+    className="flex  gap-2 md:text-base hover:text-azul4 duration-300 text-sm 
+    hover:cursor-default cursor-default ease-in-out items-center">
         <FontAwesomeIcon icon={faCircleCheck} className="text-xs"/>
         <h3 className="">{text}</h3>
     </div>
